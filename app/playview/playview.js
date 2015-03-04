7aa592da660f44f2da47255d7e8dabb5a8246d09
neillsilva <blackvector@gmail.com>
2015-03-03 20:03:23 -0500

    Before play/pause switch

--- app/playview/playview.js
+++ app/playview/playview.js
@@ -51,19 +51,43 @@ angular.module('myApp.playview', ['ngRoute'])
     }).
     success(function(data) {
         $scope.trackUrl = data.uri
+        $scope.trackShareUrl = data.permalink_url;
         $scope.trackName = data.title;
         $scope.artist = data.user.username;
+        $scope.artistUrl = data.user.permalink_url;
         $scope.artwork = data.artwork_url.replace("large", "t500x500");
-    })
+        $scope.wave = data.waveform_url;
+        $scope.stream = data.stream_url + '?client_id=' + clientId;
+        $scope.song = new Audio();
+    });
+    $scope.playing = false;
+    $scope.play = function() {
+        $scope.playing = !$scope.playing;
+        if (!$scope.playing) {
+            $scope.song.pause();
+        } else {
+            if ($scope.song.src == '') {
+                $scope.song.src = $scope.stream;
+            }
+            $scope.song.play();
+        }
+    }
+
+    $scope.startStream = function() {
+        $scope.playing = !$scope.playing;
+        $scope.song.src = $scope.stream;
+        $scope.song.play();
+    }
+
   }
 
   getUser();
   getTrack();
 
-  SC.whenStreamingReady(function() {
-    SC.stream("/tracks/190984415", function(sound) {
-        sound.play();
-    });
-  });
+  // SC.whenStreamingReady(function() {
+  //   SC.stream("/tracks/190984415", function(sound) {
+  //       sound.play();
+  //   });
+  // });
 
 }]);
